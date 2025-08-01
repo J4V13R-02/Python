98301b8dd432d677f36d68afc8e31cb29675567c
J4V13R-02 <javierromdom@gmail.com>
2025-08-01 18:04:35 +0200

    3sumcloses por fin funciona. La cabeza me echa humo.

--- 15. 3Sum.py
+++ 15. 3Sum.py
@@ -1,16 +1,19 @@
-nums = [-1,0,1,2,-1,-4]
-conjuntos = []
-visto = {}
+import random
 
-def sum(nums):
-    combinación = []
+nums = [random.randint(-5, 5) for _ in range(10)]
+conjuntos = []
+visto = set()
+
+def suma(nums):
     for i in range (len(nums)):
         for j in range (i+1, len(nums)):
-            buscado = (nums[i] + nums[j]) * -1
-            if buscado in nums and combinacion not in visto:
-                combinacion = sorted([nums[i], nums[j], buscado])
-                conjuntos.append(combinacion)
-                visto.append = combinacion
+            buscado = -(nums[i] + nums[j])
+            for k in range (j+1, len(nums)):
+                if buscado == nums[k]:
+                    combinacion = tuple(sorted([nums[i], nums[j], buscado]))
+                    if combinacion not in visto:
+                        conjuntos.append(list(combinacion))
+                        visto.add(combinacion)
     return conjuntos
-
-print(sum(nums))
+print(nums)
+print(suma(nums))
---
+++ 16-3SumClosest.py
@@ -0,0 +1,29 @@
+import random
+
+nums = [random.randint(-50, 50) for _ in range(10)]
+buscado = int(input("Número a buscar: "))
+visto = set()
+diferencia_max = 50
+mejor = 0
+def suma(nums, diferencia_max):
+    for i in nums:
+       visto.add(i)
+       for j in nums:
+           if j not in visto:
+                objetivo = buscado - (i + j)
+                visto.add(j)
+                if objetivo in nums:
+                    return objetivo
+                else:
+                    for k in nums:
+                        if k not in visto:
+                            visto.add(k)
+                            diferencia = abs(objetivo - k)
+                            if diferencia < diferencia_max:
+                                diferencia_max = diferencia
+                                mejor = i + j + k
+                                mejor_comb = [i, j, k]
+    return mejor, mejor_comb
+
+print(nums)
+print(suma(nums, diferencia_max=1000))
