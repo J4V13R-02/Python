4b7ad28a74720f950353a43d5317501727f15f98
J4V13R-02 <javierromdom@gmail.com>
2025-08-06 19:21:26 +0200

    Fibonacci y calcular phi

--- FibonacciAndPhi.py
+++ FibonacciAndPhi.py
@@ -1,14 +1,13 @@
 fibo = [0, 1, 1]
 limite = 100
-mejor_phi = 0
 
-def fibonacci(i, fibo, limite):
+def fibonacci(i, fibo):
     fibo.append(fibo[i] + fibo[i - 1])
     return fibo[i + 1]
 
-def phi(i, fibo, limite):
+def phi(i, fibo):
     mejor_phi = fibo[i] / fibo[i - 1]
     return mejor_phi
 
 for i in range(2, limite):
-    print(f"Posicion {i} Para: {fibonacci(i, fibo, limite)} phi es igual a: {phi(i, fibo, limite)}")
+    print(f"Posicion {i} Para: {fibonacci(i, fibo)} phi es igual a: {phi(i, fibo)}")
